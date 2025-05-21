94dde67684423672d20ee0031b4a561a4a19b825
lekrause <leon.krause@strato.de>
2025-05-21 11:28:05 +0200

    fixed wrong input request

--- DateChecker.ps1
+++ DateChecker.ps1
@@ -1,22 +1,34 @@
 $todaysdate = Read-Host "use todays date? [y] = yes , [n] = no"
-while($true){
-    if($todaysdate -eq "y"){
-        $date = (Get-Date).date
-        break;
-    }
+$retryDateInput = $true
 
-    elseif($todaysdate -eq "n"){
-        $date = Read-Host "set date(dd.MM.yyyy): "
-        try{
+while($true)
+{
+    if($todaysdate -eq "y")
+    {
+        $date = (Get-Date).date
+        break
+    }
+    elseif ($todaysdate -eq "n")
+    {
+        if ($retryDateInput)
+        {
+            $date = Read-Host "set date(dd.MM.yyyy)"
+        }
+        try
+        {
             $date = [datetime]::ParseExact($date, 'dd.MM.yyyy', $null)  
             break
         }
-        catch{
-        $date = Read-Host "wrong format(dd.MM.yyyy, f.e. 24.12.2024): "
-        }
+        catch
+        {
+            $date = Read-Host "wrong format(dd.MM.yyyy, f.e. 24.12.2024)"
+            $retryDateInput = $false
+        } 
     }
-    
-    else{
-        $todaysdate = Read-Host "wrong input, please [y] = yes , [n] = no : "
+    else
+    {
+        $todaysdate = Read-Host "wrong input, please [y] = yes , [n] = no"
     }
 }
+
+Write-Host "Your date is $($date.ToString('dd.MM.yyyy'))."
