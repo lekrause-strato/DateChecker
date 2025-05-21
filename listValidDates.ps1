88e69fdc363c7b9bd4d86c162c5f00dc128ef5b6
lekrause <leon.krause@strato.de>
2025-05-21 15:49:25 +0200

    fixed .csv file dates not showing up

--- listValidDates.ps1
+++ listValidDates.ps1
@@ -26,15 +26,18 @@ if (Test-Path "*.csv")
     $datesInFileCsv = Import-Csv "*.csv"
     foreach ($line in $datesInFileCsv)
     {
-        if ($line.tag -and $line.monat -and $line.jahr)
+        if ($line.day -and $line.month -and $line.year)
         {
-            $dateString = "$($line.tag).$($line.monat).$($line.jahr)"
+            $dateString = "$($line.day).$($line.month).$($line.year)"
             try
             {
                 $parsed = [datetime]::ParseExact($dateString, 'd.M.yyyy', $null)
                 $validDates += $parsed
             }
-            catch { }
+            catch
+            {
+
+            }
         }
     }
 }
@@ -44,4 +47,13 @@ else
 }
 
 $sortedDates = $validDates | Sort-Object
-$sortedDates | ForEach-Object { $_.ToString("dd-MM-yyyy") }
+$sortedDates | ForEach-Object { $_.ToString("dd.MM.yyyy") } | Out-File "sortedDates.csv"
+
+if (Test-Path "sortedDates.csv")
+{
+    Write-Host "'sortedDates.csv' file was created"
+} 
+else
+{
+    Write-Host "'sortedDates.csv' file could not be created"
+}
